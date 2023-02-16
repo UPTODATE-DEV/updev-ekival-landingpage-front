d11fe257db291d1247f4fd361864be49f9475195
Luccin Masirika <luccinmasirika@gmail.com>
2023-02-16 16:57:43 +0200

    split

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-const NavBar = dynamic(import("../components/navbar/NavBar"), { ssr: false });
-const Banner = dynamic(import("../components/banner/Banner"), { ssr: false });
-const Values = dynamic(import("../components/values/Values"), { ssr: false });
-const Convert = dynamic(import("../components/convert/Convert"), { ssr: false });
-const Features = dynamic(import("../components/features/Features"), { ssr: false });
-const Ekival = dynamic(import("../components/ekival/Ekival"), { ssr: false });
-const Team = dynamic(import("../components/team/Team"), { ssr: false });
-const Footer = dynamic(import("../components/footer/Footer"), { ssr: false });
-const Timeline = dynamic(import("../components/timeline/Timeline"), { ssr: false });
-const Partners = dynamic(import("../components/partners/Partners"), { ssr: false });
+const HomePage = dynamic(import("../screens/Home"), { ssr: false, loading: () => <p>Loading...</p> });
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <>
       <Head>
@@ -24,16 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
-      <Banner />
-      <Values />
-      <Convert />
-      <Features />
-      <Ekival />
-      <Timeline />
-      <Partners />
-      <Team />
-      <Footer />
+      <HomePage />
     </>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,32 @@
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const NavBar = dynamic(import("../components/navbar/NavBar"), { ssr: false });
+const Banner = dynamic(import("../components/banner/Banner"), { ssr: false });
+const Values = dynamic(import("../components/values/Values"), { ssr: false });
+const Convert = dynamic(import("../components/convert/Convert"), { ssr: false });
+const Features = dynamic(import("../components/features/Features"), { ssr: false });
+const Ekival = dynamic(import("../components/ekival/Ekival"), { ssr: false });
+const Team = dynamic(import("../components/team/Team"), { ssr: false });
+const Footer = dynamic(import("../components/footer/Footer"), { ssr: false });
+const Timeline = dynamic(import("../components/timeline/Timeline"), { ssr: false });
+const Partners = dynamic(import("../components/partners/Partners"), { ssr: false });
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <>
+      <NavBar />
+      <Banner />
+      <Values />
+      <Convert />
+      <Features />
+      <Ekival />
+      <Timeline />
+      <Partners />
+      <Team />
+      <Footer />
+    </>
+  );
+}
